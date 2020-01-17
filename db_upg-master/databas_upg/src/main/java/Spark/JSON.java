68e6e789526438c03a25cd302742696c00407acc
LukasRos1 <48435119+LukasRos1@users.noreply.github.com>
2020-01-17 01:38:48 +0100

    Add files via upload

---
+++ db_upg-master/databas_upg/src/main/java/Spark/JSON.java
@@ -0,0 +1,133 @@
+package Spark;
+
+/**
+ * @author Lukas Rosberg
+ * Klass som skapar objekt med information som behövs till html-sidor
+ * Flera olika konstruktorer 
+ */
+
+public class JSON {
+
+	private int estimated_time;
+	public int getSeats() {
+		return seats;
+	}
+
+	public void setSeats(int seats) {
+		this.seats = seats;
+	}
+
+	public String getArrival() {
+		return arrival;
+	}
+
+	public void setArrival(String arrival) {
+		this.arrival = arrival;
+	}
+
+	public String getDeparture() {
+		return departure;
+	}
+
+	public void setDeparture(String departure) {
+		this.departure = departure;
+	}
+
+	public String getDay_of_week() {
+		return day_of_week;
+	}
+
+	public void setDay_of_week(String day_of_week) {
+		this.day_of_week = day_of_week;
+	}
+
+	private String city2;
+	private int price;
+	private String city1;
+	private int seats;
+	private String arrival;
+	private String departure;
+	private String day_of_week;
+	private int stretchID;
+	private int tripID;
+	private int week;
+	
+	public int getWeek() {
+		return week;
+	}
+
+	public void setWeek(int week) {
+		this.week = week;
+	}
+
+	public int getTripID() {
+		return tripID;
+	}
+
+	public void setTripID(int tripID) {
+		this.tripID = tripID;
+	}
+
+	public int getStretchID() {
+		return stretchID;
+	}
+
+	public void setStretchID(int stretchID) {
+		this.stretchID = stretchID;
+	}
+
+	public JSON (String city1, String city2, int price, int seats, String arrival, String departure, String day_of_week, int stretchID, int tripID, int week) {
+		this.city1=city1;
+		this.city2=city2;
+		this.price=price;
+		this.seats=seats;
+		this.arrival=arrival;
+		this.departure=departure;
+		this.day_of_week=day_of_week;
+		this.stretchID = stretchID;
+		this.tripID=tripID;
+		this.week = week;
+	}
+	
+	public int getEstimated_time() {
+		return estimated_time;
+	}
+
+	public void setEstimated_time(int estimated_time) {
+		this.estimated_time = estimated_time;
+	}
+
+	public String getCity2() {
+		return city2;
+	}
+
+	public void setCity2(String city2) {
+		this.city2 = city2;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getCity1() {
+		return city1;
+	}
+
+	public void setCity1(String city1) {
+		this.city1 = city1;
+	}
+
+	public JSON(String string, String string2, int int1, int int2) {
+		this.city1=string;
+		this.city2=string2;
+		this.estimated_time=int1;
+		this.price=int2;
+	}
+
+
+    
+}
